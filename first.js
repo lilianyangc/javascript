bd274984da90008290f9d94db32170f4cd2d0f5a
lilianyangc <mlilianyang@gmail.com>
2020-05-06 14:39:10 -0400

    Updated functional game scripts

---
+++ first.js
@@ -0,0 +1,17 @@
+words :[
+    {category: "colors", word:"goldenrod"},
+    {category: "colors", word:"lavender"},
+    {category: "colors", word:"periwinkle"},
+    {category: "colors", word:"rainbow"},
+    {category: "colors", word:"tangerine"},
+    {category: "food", word:"cornflakes"},
+    {category: "food", word:"chocolate"},
+    {category: "food", word:"blueberry"},
+    {category: "food", word:"buckwheat"},
+    {category: "food", word:"cauliflower"},
+    {category: "weather", word:"chinook"},
+    {category: "weather", word:"cumulonimbus"},
+    {category: "weather", word:"hurricane"},
+    {category: "weather", word:"precipitation"},
+    {category: "weather", word:"whirlwind"}
+    ]
